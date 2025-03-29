297ca8c991ef86bbf4a1b2995b11cf98b7e260fe
Daniel Bohry <danielbohry@gmail.com>
2025-03-29 11:29:32 +0100

    update task executor config

--- src/main/java/com/lhamacorp/springocrtesseract/config/AsyncConfig.java
+++ src/main/java/com/lhamacorp/springocrtesseract/config/AsyncConfig.java
@@ -4,19 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
 
 @Configuration
 public class AsyncConfig {
 
-    @Bean
-    public ExecutorService executorService() {
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(50);
         executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("OCRService-");
         executor.initialize();
-        return executor.getThreadPoolExecutor();
+        return executor;
     }
 }
